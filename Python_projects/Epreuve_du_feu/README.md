64518942e0a9c786beb9096e34895325c4adaa1e
Hamza <hamzabenreguig@gmail.com>
2020-03-17 16:11:23 +0100

    Update caps.py

---
+++ Python_projects/Epreuve_du_feu/README.md
@@ -0,0 +1,13 @@
+anagram.py:
+
+    Anagram is program that takes a word and a file 
+    and gives you anagram of your word base on the word he find on the file.
+
+caps.py:
+
+    He caps one out of two letters of your sentence
+
+factorial.py:
+
+
+
--- Python_projects/Epreuve_du_feu/caps.py
+++ Python_projects/Epreuve_du_feu/caps.py
@@ -1,14 +1,25 @@
 #Exo 2
 import sys
-def Maj(string):
-    new_string = ''
 
-    for i in range(len(string)):
-        if(i % 2 == 0):
-            new_string += string[i].upper()
+def Maj(*arg):
+    sentence = ''
+    new_string = ''
+    len_lst = []
+    for string in arg[0]:
+        len_lst.append(len(string))
+        sentence += string 
+    
+    for i in range(len(sentence)):
+        if i % 2 == 0:
+            new_string += sentence[i].lower()
         else:
-            new_string += string[i].lower()
-    return new_string
+            new_string += sentence[i].upper()
+
+    for e in len_lst:
+        new_string = new_string[e:] + ' ' + new_string[:e]
+
+
+    return new_string[1:]
 
 if __name__ == "__main__":
-    print(Maj(sys.argv[1]))
+    print(Maj(sys.argv[1:]))
